56d6535be23340abc8998d481143fee8af9f440a
arrowira <41136800+arrowira@users.noreply.github.com>
2026-07-17 17:03:24 -0700

    normalized movement

--- kenney-jam/Scripts/PlayerMovement.gd
+++ kenney-jam/Scripts/PlayerMovement.gd
@@ -3,15 +3,18 @@ extends CharacterBody3D
 var Speed = 2
 
 func _physics_process(delta: float) -> void:
+	var dir = Vector3.ZERO
 	
 	if(Input.is_action_pressed("left")):
-		velocity.x-=Speed
+		dir.x-=Speed
 	if(Input.is_action_pressed("right")):
-		velocity.x+=Speed
+		dir.x+=Speed
 	if(Input.is_action_pressed("up")):
-		velocity.z-=Speed
+		dir.z-=Speed
 	if(Input.is_action_pressed("down")):
-		velocity.z+=Speed
+		dir.z+=Speed
+	
+	velocity += dir.normalized()
 		
-	velocity-=velocity*0.1
+	velocity-=velocity*0.2
 	move_and_slide()
