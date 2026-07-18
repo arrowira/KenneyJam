7c9ad507cc8a40cfe346b487abc314eca80d1881
arrowira <41136800+arrowira@users.noreply.github.com>
2026-07-18 01:08:14 -0700

    scale attributes

--- kenney-jam/Scripts/ScaleScript.gd
+++ kenney-jam/Scripts/ScaleScript.gd
@@ -1,6 +1,11 @@
 extends Area3D
 
+var rarity = 0
 func _on_body_entered(body: Node3D) -> void:
 	if(body.name == "Player"):
 		body.Scales +=1
 		queue_free()
+
+func configure(color, r):
+	$Sprite.modulate = color
+	rarity = r
