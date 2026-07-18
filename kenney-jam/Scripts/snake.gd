49522bf67536a27f9fb8a3ba81076efcf8b7529e
arrowira <41136800+arrowira@users.noreply.github.com>
2026-07-17 23:53:07 -0700

    snake idle slowdown logic

--- kenney-jam/Scripts/snake.gd
+++ kenney-jam/Scripts/snake.gd
@@ -3,21 +3,23 @@ extends Node3D
 
 var dir = Vector2(1,0)
 var behavior = "idle"
-var SPEED = 200
+var SPEED = 0.01
 
 func _physics_process(delta: float) -> void:
 	if behavior == "walk":
 		position.x += dir.x*SPEED
 		position.z += dir.y*SPEED
-		
+	elif behavior == "idle":
+		$AnimationPlayer.speed_scale = $idleTime.time_left/$idleTime.wait_time
 
 func _on_mood_timeout() -> void:
 	if randf()<0.8:
 		behavior = "walk"
+		$AnimationPlayer.speed_scale=1
 		$AnimationPlayer.play("slither")
 	else:
-		$AnimationPlayer.play("idle")
 		behavior = "idle"
+		$idleTime.start()
 	if behavior == "idle":
 		if randf()<0.3:
 			dir *= -1
