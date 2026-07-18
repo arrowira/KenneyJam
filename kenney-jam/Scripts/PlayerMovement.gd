584d30ccbbadfe042be6ad792ab6db05f477a3f7
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2026-07-17 23:19:51 -0700

    Rotation Fix

--- kenney-jam/Scripts/PlayerMovement.gd
+++ kenney-jam/Scripts/PlayerMovement.gd
@@ -6,14 +6,33 @@ var Speed = 1
 
 var Scales = 0
 
+var CPivot = 1
+var TPos = Vector3.ZERO
+var COff = Vector3.ZERO
+
 func _ready():
 	DScale = $Sprite.scale
+	TPos = $Camera.position
 
 func _physics_process(delta: float) -> void:
 	
 	get_parent().get_node("CanvasLayer/ScaleCount").text = str(Scales)
 	
 	var dir = Vector3.ZERO
+	if(Input.is_action_just_pressed("cright")):
+		CPivot+=1
+		if(CPivot >= 5):
+			CPivot=1
+		TPos = get_node("Pivot" + str(CPivot)).position
+		$Sprite.rotation_degrees.y+=90
+	if(Input.is_action_just_pressed("cleft")):
+		CPivot-=1
+		if(CPivot <= 0):
+			CPivot=4
+		TPos = get_node("Pivot" + str(CPivot)).position
+		$Sprite.rotation_degrees.y-=90
+	$Camera.position = $Camera.position.move_toward(TPos, 0.3)
+	$Camera.look_at(position)
 	
 	if(Input.is_action_pressed("left")):
 		dir.x-=Speed
@@ -23,7 +42,9 @@ func _physics_process(delta: float) -> void:
 		dir.z-=Speed
 	if(Input.is_action_pressed("down")):
 		dir.z+=Speed
-		
+	
+	dir = dir.rotated(Vector3.UP, $Camera.global_rotation.y)
+	
 	if(Input.is_action_pressed("crouch")):
 		velocity*=0.5
 		$Sprite.scale.y = 0.5
