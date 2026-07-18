f04ee3c9f47273fb9d5ea299786e4de5f7b69a93
arrowira <41136800+arrowira@users.noreply.github.com>
2026-07-17 23:45:15 -0700

    basic snake movement

--- kenney-jam/Scripts/snake.gd
+++ kenney-jam/Scripts/snake.gd
@@ -3,14 +3,20 @@ extends Node3D
 
 var dir = Vector2(1,0)
 var behavior = "idle"
+var SPEED = 200
 
 func _physics_process(delta: float) -> void:
-	
+	if behavior == "walk":
+		position.x += dir.x*SPEED
+		position.z += dir.y*SPEED
+		
 
 func _on_mood_timeout() -> void:
 	if randf()<0.8:
 		behavior = "walk"
+		$AnimationPlayer.play("slither")
 	else:
+		$AnimationPlayer.play("idle")
 		behavior = "idle"
 	if behavior == "idle":
 		if randf()<0.3:
