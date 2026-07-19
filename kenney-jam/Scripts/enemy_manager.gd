49417994560d01446cd16e0f4a090d26f3b2dc6f
arrowira <41136800+arrowira@users.noreply.github.com>
2026-07-19 02:06:26 -0700

    snake despawning

--- kenney-jam/Scripts/enemy_manager.gd
+++ kenney-jam/Scripts/enemy_manager.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @export var frequency = 1
-var maxEnemies = 3
+var maxEnemies = 20
 var enemies = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
