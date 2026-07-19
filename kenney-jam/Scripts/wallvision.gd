9286389db2f0075354dc28d81ab57434a6f6cf24
arrowira <41136800+arrowira@users.noreply.github.com>
2026-07-18 19:34:16 -0700

    wall vision script

---
+++ kenney-jam/Scripts/wallvision.gd
@@ -0,0 +1,18 @@
+extends RayCast3D
+
+var invisWall = null
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	target_position = get_parent().get_parent().get_parent().playerPos
+	if is_colliding():
+		if get_collider().name == "wallBody":
+			if get_collider().get_parent()!=invisWall:
+				if invisWall != null:
+					invisWall.deInvis()
+				invisWall = get_collider().get_parent()
+				invisWall.invis()
+			
