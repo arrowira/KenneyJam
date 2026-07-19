2e9528a902d72a6a05a98b891ea18b84dda240b9
arrowira <41136800+arrowira@users.noreply.github.com>
2026-07-18 19:43:45 -0700

    put the stuff on the right wall scene

--- kenney-jam/Scripts/wallvision.gd
+++ kenney-jam/Scripts/wallvision.gd
@@ -9,10 +9,14 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	target_position = get_parent().get_parent().get_parent().playerPos
 	if is_colliding():
+		print(get_collider().name)
 		if get_collider().name == "wallBody":
+			
 			if get_collider().get_parent()!=invisWall:
 				if invisWall != null:
 					invisWall.deInvis()
 				invisWall = get_collider().get_parent()
 				invisWall.invis()
+		else:
+			invisWall.deInvis()
 			
